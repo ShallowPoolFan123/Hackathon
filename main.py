6aaee59ad5db9222883cbe74d36e80694737b4fd
thep6ych <mateo.at.riel@gmail.com>
2025-11-28 17:53:31 -0500

    added background

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ from ursina import *
 
 app = Ursina()
 
+background = Entity(
+    parent = camera.ui,
+    model = 'quad',
+    texture = 'assets/background.png', 
+    scale = (window.aspect_ratio, 1),
+    position = (0, 0)
+)
+
 app.run()
