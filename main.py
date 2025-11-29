888a676d6fb2b1ef79d82c8da64f155f587fef95
andrew-jpg-creator <andrew.small@acadecap.org>
2025-11-28 19:24:58 -0500

    Start Game Screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ursina import *
 app = Ursina()
-
+back = Button(text='Andrew',scale=(.2,.1),position=(-0,0.1),pressed_scale=0.95,text_size=.9)
+back2 = Button(text='Mateo',scale=(.2,.1),position=(-0,-0.1),pressed_scale=0.95,text_size=.9)
 app.run()
