61f6cb7bcb05a780dfc58f337066abbae46f4855
plopa1 <ctlabreche@gmail.com>
2025-11-29 06:36:05 -0500

    No velocity, collisions broken

--- menu.py
+++ menu.py
@@ -91,19 +91,17 @@ background = Entity(
 class Player(Sprite):
     def __init__(self, texture, position):
         super().__init__(
-            texture = texture,
-            collider = 'box',
+            texture = texture, 
             ppu = 16,
             scale = 1.2,
+            speed = 5,
             position = position
             )
+        self.collider = BoxCollider(self, center=Vec3(0,0,0), size=Vec3(.3,1,1))
         self.health = 100
 
 player1 = Player('assets/idlePlayer.png', (-4, -2, -1))
-
 player2 = Player('assets/idlePlayer2.png', (4, -2, -1))
-
-speed = 5
 
 wall1 = Entity(
     model = 'quad',
@@ -126,29 +124,63 @@ wall2 = Entity(
     )
 
 
-game1 = False
 def update():
-    if held_keys['d']:
-        player1.x += time.dt * speed
+    p1Origin = player1.world_position
+    p1hitInfoR = raycast(p1Origin, direction=Vec3(1,0,0), ignore=(player1,wall1, wall2), distance=time.dt * player1.speed, debug=False)
+    p1hitInfoL = raycast(p1Origin, direction=Vec3(-1,0,0), ignore=(player1,wall1,wall2), distance=time.dt * player1.speed, debug=False)
+    if held_keys['d']:        
+        if not p1hitInfoR.hit:
+            player1.x += time.dt * player1.speed # right
     if held_keys['a']:
-        player1.x -= time.dt * speed 
+        if not p1hitInfoL.hit:
+            player1.x -= time.dt * player1.speed # left
 
-    if held_keys['l']:
-        player2.x += time.dt * speed
+    
+
+
+    p2Origin = player2.world_position
+    p2hitInfoR = raycast(p2Origin, direction=Vec3(1,0,0), ignore=(player2,wall1, wall2), distance=time.dt * player2.speed, debug=False)
+    p2hitInfoL = raycast(p2Origin, direction=Vec3(-1,0,0), ignore=(player2,wall1,wall2), distance=time.dt * player2.speed, debug=False)
+    if held_keys['l']:        
+        if not p2hitInfoR.hit:
+            player2.x += time.dt * player2.speed # right
     if held_keys['j']:
-        player2.x -= time.dt * speed 
+        if not p2hitInfoL.hit:
+            player2.x -= time.dt * player2.speed # left
+
+
+    if p1hitInfoR.hit and p2hitInfoL.hit:
+        player1.x -= player1.speed * time.dt * 2 # left
+        player2.x += player2.speed * time.dt * 2 # right
+
+    if p1hitInfoL.hit and p2hitInfoR.hit:
+        player1.x += player1.speed * time.dt * 2 # right
+        player2.x -= player2.speed * time.dt * 2 # left
+
+    # if held_keys['l']:
+    #     player2.x += time.dt * speed
+    # if held_keys['j']:
+    #     player2.x -= time.dt * speed 
+
+    # def move(player1):
+    #     player1.direction = Vec3(
+    #         player1.right * (held_keys['d'] - held_keys['a'])).normalized()
+    #     origin = player1.world_position
+    #     hit_info = raycast(origin, player1.direction, ignore=(player1,), distance=.5, debug=False)
+    #     if not hit_info.hit:
+    #         player1.position += player1.direction * 5 * time.dt
 
     if player1.intersects(wall1).hit:
-        player1.x = -5.6
+        player1.x = -6.3
         print("hit")
     if player1.intersects(wall2).hit:
-        player1.x = 5.6
+        player1.x = 6.3
         print("hit")
     if player2.intersects(wall1).hit:
-        player2.x = -5.6
+        player2.x = -6.3
         print("hit")
     if player2.intersects(wall2).hit:
-        player2.x = 5.6
+        player2.x = 6.3
         print("hit")
 
 def exitGame():
