60a5af74c024206cd4b85b037ee5db372746c1bc
thep6ych <mateo.at.riel@gmail.com>
2025-11-28 20:11:31 -0500

    trying to move camera

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from ursina import *
+from ursina import Ursina, camera, Entity, EditorCamera
 
 app = Ursina(size=(1920, 1080))
+
+camera.position = Vec3(100, 0, 100)
 
 music = Audio('assets/fightMusic.mp3', loop=True, autoplay=True)
 
@@ -9,7 +12,7 @@ background = Entity(
     model = 'quad',
     texture = 'assets/gameBackground.png', 
     scale = (window.aspect_ratio, 1),
-    position = (0, 0)
+    position = (0, 0.5)
 )
 
 app.run()
