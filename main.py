b279941e1e85c98d9d4ac0b918fb06c2cc2ba745
thep6ych <mateo.at.riel@gmail.com>
2025-11-28 21:43:00 -0500

    added 2 moving players

--- main.py
+++ main.py
@@ -6,28 +6,49 @@ app = Ursina(size=(1280, 720))
 
 music = Audio('assets/fightMusic.mp3', loop=True, autoplay=True)
 
+
+
 background = Entity(
-    parent = camera.ui,
-    model = 'quad',
-    color = color.violet,
-    texture = 'assets/gameBackground.png', 
-    scale = (window.aspect_ratio, 1),
-    position = (0, 0)
+      model = 'quad',
+      color = color.violet,
+      texture = 'assets/gameBackground.png', 
+      scale = (15.3, 8.2, .000001),
 )
 
-player1 = Entity(
-    model='assets/playerIdle',
-    scale = 1,
-    color = color.red,
-    origin = (1, -0.5)
+player1 = Sprite(
+    texture='assets/playerIdle.png',
+    ppu = 16,
+    scale = 0.5,
+    origin = (1, 0.5, 5)
     )
 
 speed = 5
 
 def update():
     if held_keys['d']:
-        player1.X += time.dt * speed
+        player1.x += time.dt * speed
     if held_keys['a']:
-        player1.X -= time.dt * speed 
+        player1.x -= time.dt * speed 
+    if held_keys['w']:
+        player1.y += time.dt * speed 
+    if held_keys['s']:
+        player1.y -= time.dt * speed 
+
+    if held_keys['l']:
+        player2.x += time.dt * speed
+    if held_keys['j']:
+        player2.x -= time.dt * speed 
+    if held_keys['i']:
+        player2.y += time.dt * speed 
+    if held_keys['k']:
+        player2.y -= time.dt * speed 
+
+player2 = Sprite(
+    texture='assets/playerIdle.png',
+    ppu = 16,
+    scale = 0.5,
+    origin = (-1, 0.5, 5)
+    )
+
 
 app.run()
