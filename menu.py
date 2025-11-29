8bc0e2c5b77c53a13fc3072e049c452262f59bd0
plopa1 <ctlabreche@gmail.com>
2025-11-28 20:45:51 -0500

    Fixed credits
    i fixed the credits :3

--- menu.py
+++ menu.py
@@ -3,26 +3,23 @@ import subprocess
 import sys
 import os
 import time
+
+
 app = Ursina(size=(1920, 1080))
 music = Audio('assets/bossTime.mp3', loop=True, autoplay=True)
-e=Button(text='e',world_position=(16.3,10))
+
+
+### Objects ###
+
+# Menu
+
 background = Entity(
     parent = camera.ui,
     model = 'quad',
     texture = 'assets/background.png', 
     scale = (window.aspect_ratio, 1),
     position = (0, 0))
-def startCredits():
-    print("Attempting to start external credits...")
-    python_executable = sys.executable
-    script_path = os.path.join(os.path.dirname(__file__), 'credits.py')
-    try:
-        subprocess.Popen([python_executable, script_path])
-        print("External program launched.")
-        time.sleep(1)
-        quit()
-    except Exception as e:
-        print(f"Failed to launch program: {e}")
+
 creditsButton = Button(
     text = "Play Credits",
     color = color.blue,
@@ -30,6 +27,56 @@ creditsButton = Button(
     scale = (0.3,0.2),
     pressed_sound = 'assets/sword_slash.mp3',
     text_size = 1.5,)
+
+mainButton = Button(
+    text = "Play Game",
+    color = color.green,
+    origin = (-2, 0),
+    scale = (0.3,0.2),
+    pressed_sound = 'assets/sword_slash.mp3',
+    text_size = 1.5,)
+
+quitButton = Button(
+    text = 'Quit',
+    color=color.red,
+    origin=(-2,-1.5),
+    scale=(.3,.2),
+    pressed_sound = 'assets/sword_slash.mp3',
+    text_size=1.5
+)
+
+### Credits ###
+def startCredits():
+
+    creditsText = Text(
+        text='Coders: Mateo, Cedric, Andrew\n\nArt: Max, Quinn, Andrew',
+        size=4,
+        origin=(0,0)
+    )
+
+    creditsBack = Button(
+        text= 'Back',
+        scale = (1.8, 0.1),
+        origin = (0, 4),
+        pressed_scale = 0.95,
+        text_size = 0.9,
+    )
+    
+
+    def deleteCredits():
+            destroy(creditsText)
+            destroy(creditsBack)
+    creditsBack._on_click = deleteCredits
+
+
+
+### Functions ###
+
+# Menu
+
+
+    
+
 def startGame():
     print("Attempting to start game...")
     python_executable = sys.executable
@@ -41,24 +88,15 @@ def startGame():
         quit()
     except Exception as e:
         print(f"Failed to launch program: {e}")
-mainButton = Button(
-    text = "Play Game",
-    color = color.green,
-    origin = (-2, 0),
-    scale = (0.3,0.2),
-    pressed_sound = 'assets/sword_slash.mp3',
-    text_size = 1.5,)
+
 def exitGame():
     time.sleep(1)
     quit()
-quitButton = Button(
-    text = 'Quit',
-    color=color.red,
-    origin=(-2,-1.5),
-    scale=(.3,.2),
-    pressed_sound = 'assets/sword_slash.mp3',
-    text_size=1.5
-)
+
+
+
+
+
 
 creditsButton._on_click = startCredits
 mainButton._on_click = startGame
