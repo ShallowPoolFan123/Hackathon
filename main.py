e6910d880de2930624ec0d5fedd2b503bf37b9eb
thep6ych <mateo.at.riel@gmail.com>
2025-11-28 20:21:40 -0500

    fixed screen

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ from ursina import Ursina, camera, Entity, EditorCamera
 
 app = Ursina(size=(1920, 1080))
 
-camera.position = Vec3(100, 0, 100)
-
 music = Audio('assets/fightMusic.mp3', loop=True, autoplay=True)
 
 background = Entity(
     parent = camera.ui,
     model = 'quad',
-    #texture = 'assets/background.png', 
     color=color.red,
     texture = 'assets/gameBackground.png', 
     scale = (window.aspect_ratio, 1),
-    position = (0, 0.5)
+    position = (0, 0)
 )
 
 # Classes
